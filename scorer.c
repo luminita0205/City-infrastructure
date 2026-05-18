8f1046c4bf38f1f13d9b16c3a486aa5c610a26a1
luminita0205 <mluminita62@yahoo.com>
2026-05-18 16:51:58 +0300

    Add scorer program for report scores

---
+++ scorer.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include<string.h>
+#include <fcntl.h>
+#define MAX 100
+typedef struct
+{
+    int report_id;
+    char inspector_name[30];
+    float latitude;
+    float longitude;
+    char issue_category[30];
+    int severity_level;
+    time_t timp;
+    char description[100];
+} Report;
+//for each score we need the name of the inspector and the severity level(the sum will be the nwe score)
+typedef struct
+{
+    char name[30];
+    int score;
+}Score;
+int main(int argc,char *argv[])
+{
+    if(argc !=2)
+    {
+        printf("Invalid arguments");
+        exit(-1);
+    }
+    //reads each report from the current district
+    char path[100];
+    sprintf(path,"%s/reports.dat",argv[1]);
+
+    FILE *f=fopen(path,"rb");
+    if(f==NULL)
+    {
+        printf("error opening file");
+        exit(-1);
+
+    }
+
+    Score s[MAX];
+    //number of inspectors
+    int n=0;
+
+    Report report;
+    while(fread(&report,sizeof(Report),1,f)==1)
+    {
+        int index=-1;
+
+        for(int i=0;i<n;i++)
+        {
+            if(strcmp(s[i].name,report.inspector_name)==0)
+            {
+                index=i;
+            }
+        }
+
+        if(index==-1)
+        {
+            strcpy(s[n].name,report.inspector_name);
+            s[n].score=report.severity_level;
+            n++;
+        }
+        else
+        {
+            //if the inspector already exists the new score is score+=severity
+            s[index].score+=report.severity_level;
+        }
+    }
+    fclose(f);
+    printf("District: %s\n", argv[1]);
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%s %d\n", s[i].name, s[i].score);
+    }
+    return 0;
+}
